c1c96b81381d2dbc7b993ad63e51e710cf518ca3
karenok <karenokonkwo29@gmail.com>
2021-02-14 00:49:09 +0100

    Fix: overlapping sections

--- src/components/ContactPage/ContactPage.js
+++ src/components/ContactPage/ContactPage.js
@@ -66,99 +66,103 @@ const ContactPage = () => {
 					I’m always interested in hearing about new projects and opportunities.
 				</p>
 			</header>
-			<div className="contact-page__content">
-				<div className="contact-page__content-left">
-					<h2 className="contact-page__heading">
-						Let's talk about everything!
-					</h2>
-					<p>
-						Don't like forms? Send me an{" "}
-						<a
-							href="mailto:karenokonkwo29@gmail.com"
-							className="contact-page__link"
-						>
-							email
-						</a>
-						. 👋
-					</p>
-				</div>
-				<div className="contact-page__content-right">
-					<form className="contact-page__form" onSubmit={onSubmit}>
-						<input
-							className="contact-page__input"
-							placeholder="Your name"
-							name="name"
-							aria-label="Your name"
-							required
-							value={formData.name}
-							onChange={onChange}
-						/>
-						<input
-							className="contact-page__input"
-							placeholder="Email address"
-							aria-label="Email address"
-							name="email"
-							type="email"
-							required
-							value={formData.email}
-							onChange={onChange}
-						/>
-						<input
-							className="contact-page__input contact-page__input--full"
-							placeholder="Subject"
-							aria-label="Subject"
-							name="subject"
-							required
-							value={formData.subject}
-							onChange={onChange}
-						/>
-						<textarea
-							className="contact-page__input contact-page__textarea"
-							placeholder="Message"
-							aria-label="Message"
-							name="message"
-							required
-							value={formData.message}
-							onChange={onChange}
-						></textarea>
+			<div className="contact-page__body">
+				<div className="contact-page__content">
+					<div className="contact-page__content-left">
+						<h2 className="contact-page__heading">
+							Let's talk about everything!
+						</h2>
+						<p>
+							Don't like forms? Send me an{" "}
+							<a
+								href="mailto:karenokonkwo29@gmail.com"
+								className="contact-page__link"
+							>
+								email
+							</a>
+							. 👋
+						</p>
+					</div>
+					<div className="contact-page__content-right">
+						<form className="contact-page__form" onSubmit={onSubmit}>
+							<input
+								className="contact-page__input"
+								placeholder="Your name"
+								name="name"
+								aria-label="Your name"
+								required
+								value={formData.name}
+								onChange={onChange}
+							/>
+							<input
+								className="contact-page__input"
+								placeholder="Email address"
+								aria-label="Email address"
+								name="email"
+								type="email"
+								required
+								value={formData.email}
+								onChange={onChange}
+							/>
+							<input
+								className="contact-page__input contact-page__input--full"
+								placeholder="Subject"
+								aria-label="Subject"
+								name="subject"
+								required
+								value={formData.subject}
+								onChange={onChange}
+							/>
+							<textarea
+								className="contact-page__input contact-page__textarea"
+								placeholder="Message"
+								aria-label="Message"
+								name="message"
+								required
+								value={formData.message}
+								onChange={onChange}
+							></textarea>
 
-						<div style={{ textAlign: "right" }}>
-							<button className="contact-page__send-btn">Send Message</button>
-						</div>
-					</form>
+							<div style={{ textAlign: "right" }}>
+								<button className="contact-page__send-btn">Send Message</button>
+							</div>
+						</form>
+					</div>
 				</div>
-			</div>
-			<div className="contact-page__content">
-				<div className="contact-page__content-left">
-					<h2 className="contact-page__heading">Reach out on social media!</h2>
-				</div>
-				<div className="contact-page__content-right">
-					<ul className="social">
-						<li className="social__item">
-							<a
-								href="https://github.com/KarenOk"
-								className="social__link social__link--active"
-							>
-								<GithubIcon className="social__icon" />
-								Github
-							</a>
-						</li>
-						<li className="social__item">
-							<a
-								href="https://www.linkedin.com/in/karenokonkwo/"
-								className="social__link"
-							>
-								<LinkedinIcon className="social__icon" fill="#0072b1" />
-								LinkedIn
-							</a>
-						</li>
-						<li className="social__item">
-							<a href="https://www.twitter.com/" className="social__link">
-								<TwitterIcon className="social__icon" fill="#1DA1F2" />
-								Twitter
-							</a>
-						</li>
-					</ul>
+				<div className="contact-page__content">
+					<div className="contact-page__content-left">
+						<h2 className="contact-page__heading">
+							Reach out on social media!
+						</h2>
+					</div>
+					<div className="contact-page__content-right">
+						<ul className="social">
+							<li className="social__item">
+								<a
+									href="https://github.com/KarenOk"
+									className="social__link social__link--active"
+								>
+									<GithubIcon className="social__icon" />
+									Github
+								</a>
+							</li>
+							<li className="social__item">
+								<a
+									href="https://www.linkedin.com/in/karenokonkwo/"
+									className="social__link"
+								>
+									<LinkedinIcon className="social__icon" fill="#0072b1" />
+									LinkedIn
+								</a>
+							</li>
+							<li className="social__item">
+								<a href="https://www.twitter.com/" className="social__link">
+									<TwitterIcon className="social__icon" fill="#1DA1F2" />
+									Twitter
+								</a>
+							</li>
+						</ul>
+					</div>
 				</div>
 			</div>
 		</section>

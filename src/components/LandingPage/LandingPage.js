cb8ae129422aa87d3f6aee89792e95045cfd1fe0
karenok <karenokonkwo29@gmail.com>
2021-02-13 05:34:03 +0100

    Add content to landing page

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import "./LandingPage.css";
-import logo from "../../logos/logo6 animated.svg";
+import logo from "../../logos/logo6 white animated.svg";
 import Banner1 from "../../images/banner 1.jpeg";
 import Banner2 from "../../images/banner 2.jpeg";
+import Typewriter from "typewriter-effect";
 
 const banners = [Banner1, Banner2];
 
@@ -10,7 +11,7 @@ const LandingPage = () => {
 	const [banner, setBanner] = useState(0);
 
 	useEffect(() => {
-		const timer = setTimeout(nextBanner, 2000);
+		const timer = setTimeout(nextBanner, 3000);
 
 		return () => {
 			clearTimeout(timer);
@@ -19,7 +20,6 @@ const LandingPage = () => {
 
 	const nextBanner = () => {
 		let next = banner + 1;
-		console.log(next);
 		setBanner(next > banners.length - 1 ? 0 : next);
 	};
 
@@ -28,8 +28,66 @@ const LandingPage = () => {
 			className="landing-page"
 			style={{ backgroundImage: `url('${banners[banner]}')` }}
 		>
-			{/* <object data={logo} className="App-logo" /> */}
 			<div className="landing-page__overlay"></div>
+			<header className="landing-page__header">
+				<a href="/">
+					<object
+						data={logo}
+						className="landing-page__logo"
+						type="image/svg+xml"
+					/>
+				</a>
+			</header>
+			<div className="landing-page__body">
+				<div className="landing-page__text-cont">
+					<section className="landing-page__intro">
+						<h1 className="landing-page__name"> Karen Okonkwo </h1>
+
+						<Typewriter
+							options={{
+								strings: ["Software Engineer.", "Full-Stack Web Developer."],
+								autoStart: true,
+								loop: true,
+								wrapperClassName: "landing-page__role",
+								loop: true,
+								cursorClassName: "landing-page__cursor",
+							}}
+						/>
+					</section>
+					<nav className="landing-page__nav">
+						<ul className="landing-page__nav-list">
+							<li className="landing-page__nav-item">
+								<a
+									href="#!"
+									className="landing-page__nav-link landing-page__nav-link--active"
+								>
+									Home
+								</a>
+							</li>
+							<li className="landing-page__nav-item">
+								<a href="#!" className="landing-page__nav-link">
+									About
+								</a>
+							</li>
+							<li className="landing-page__nav-item">
+								<a href="#!" className="landing-page__nav-link">
+									Resume
+								</a>
+							</li>
+							<li className="landing-page__nav-item">
+								<a href="#!" className="landing-page__nav-link">
+									Portfolio
+								</a>
+							</li>
+							<li className="landing-page__nav-item">
+								<a href="#!" className="landing-page__nav-link">
+									Contact
+								</a>
+							</li>
+						</ul>
+					</nav>
+				</div>
+			</div>
 		</div>
 	);
 };

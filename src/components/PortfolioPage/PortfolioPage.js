84a39fa34568a827b7b83be7f6293513fa5bf4aa
karenok <karenokonkwo29@gmail.com>
2021-02-14 23:25:36 +0100

    Integrate modal and implement full screen zoom

--- src/components/PortfolioPage/PortfolioPage.js
+++ src/components/PortfolioPage/PortfolioPage.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./PortfolioPage.css";
-import { AboutIcon, GithubIcon } from "../../images/icons/icons";
+import { AboutIcon, CloseIcon, ZoomIcon } from "../../images/icons/icons";
 import project from "../../images/project.png";
 import project2 from "../../images/project2.png";
 import data from "../../data.json";
+import Modal from "../Modal/Modal";
 
 const PortfolioPage = () => {
+	const [showModal, setShowModal] = useState(false);
+	const [selectedImg, setSelectedImg] = useState(null);
+	const [showFullscreen, setShowFullscreen] = useState(false);
+
+	useEffect(() => {
+		if (selectedImg) setShowFullscreen(true);
+		else setShowFullscreen(false);
+	}, [selectedImg]);
+
 	return (
 		<section className="portfolio-page">
+			<Modal visible={showModal} close={() => setShowModal(false)} />
 			<header className="portfolio-page__header">
 				<h1 className="portfolio-page__heading portfolio-page__heading--main">
 					Recent Works
@@ -18,7 +29,11 @@ const PortfolioPage = () => {
 			</header>
 			<div className="portfolio-page__body">
 				{new Array(10).fill(0).map((item, i) => (
-					<div className="portfolio-page__img-wrapper">
+					<div
+						className="portfolio-page__img-wrapper"
+						key={i}
+						// onClick={() => setShowModal(true)}
+					>
 						{i % 2 === 0 ? (
 							<>
 								<img
@@ -28,15 +43,30 @@ const PortfolioPage = () => {
 								/>
 								<div className="portfolio-page__desc">
 									<h3> Scribbles Blog </h3>
-									<button
-										className="portfolio-page__img-btn"
-										aria-label="About scribbles blog"
-									>
-										<AboutIcon
-											width={40}
-											className="portfolio-page__img-icon"
-										/>
-									</button>
+									<div className="portfolio-page__actions">
+										<button
+											className="portfolio-page__img-btn"
+											aria-label="View fullscreen"
+											title="View fullscreen"
+											onClick={() => setSelectedImg(project)}
+										>
+											<ZoomIcon
+												width={40}
+												className="portfolio-page__img-icon"
+											/>
+										</button>
+										<button
+											className="portfolio-page__img-btn"
+											aria-label="View details"
+											title="View details"
+											onClick={() => setShowModal(true)}
+										>
+											<AboutIcon
+												width={40}
+												className="portfolio-page__img-icon"
+											/>
+										</button>
+									</div>
 								</div>
 							</>
 						) : (
@@ -48,20 +78,59 @@ const PortfolioPage = () => {
 								/>
 								<div className="portfolio-page__desc">
 									<h3> Scribbles Blog </h3>
-									<button
-										className="portfolio-page__img-btn"
-										aria-label="About scribbles blog"
-									>
-										<AboutIcon
-											width={40}
-											className="portfolio-page__img-icon"
-										/>
-									</button>
+									<div className="portfolio-page__actions">
+										<button
+											className="portfolio-page__img-btn"
+											aria-label="View fullscreen"
+											title="View fullscreen"
+											onClick={() => setSelectedImg(project2)}
+										>
+											<ZoomIcon
+												width={40}
+												className="portfolio-page__img-icon"
+											/>
+										</button>
+										<button
+											className="portfolio-page__img-btn"
+											aria-label="View details"
+											title="View details"
+											onClick={() => setShowModal(true)}
+										>
+											<AboutIcon
+												width={40}
+												className="portfolio-page__img-icon"
+											/>
+										</button>
+									</div>
 								</div>
 							</>
 						)}
 					</div>
 				))}
+
+				{showFullscreen ? (
+					<div className="portfolio-page__img-wrapper--full">
+						<button
+							className="portfolio-page__close-btn"
+							aria-label="Close fullscreen"
+							onClick={() => setSelectedImg(null)}
+						>
+							<CloseIcon
+								width={50}
+								height={50}
+								stroke="white"
+								className="portfolio-page__close-icon"
+							/>
+						</button>
+						<img
+							src={selectedImg}
+							alt="Project"
+							className="portfolio-page__img"
+						/>
+					</div>
+				) : (
+					<div />
+				)}
 			</div>
 		</section>
 	);

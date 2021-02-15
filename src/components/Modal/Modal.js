153a78c9ca935208e5f1c9afd88e02d3a862c57f
karenok <karenokonkwo29@gmail.com>
2021-02-15 01:17:18 +0100

    Use dynamic portfolio data

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -3,31 +3,53 @@ import "./Modal.css";
 import Rodal from "rodal";
 import "rodal/lib/rodal.css";
 import { GithubIcon, LiveIcon } from "../../images/icons/icons";
-import project2 from "../../images/project2.png";
 
-const Modal = ({ visible, close }) => {
+const Modal = ({ visible, close, project }) => {
+	if (!project) return <div />;
+
 	return (
 		<Rodal visible={visible} onClose={close}>
 			<div className="modal__img-wrapper">
-				<img src={project2} alt="Project" className="modal__img" />
+				{project.gif_url ? (
+					<img
+						src={project.gif_url}
+						alt={project.name}
+						className="modal__img"
+					/>
+				) : (
+					<img
+						src={project.image_url}
+						alt={project.name}
+						className="modal__img"
+					/>
+				)}
 			</div>
-			<h1 className="modal__heading"> Scribbles Blog</h1>
-			<p className="modal__summary">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-				veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-				commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-				velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-				occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-				mollit anim id est laborum.
-			</p>
+			<h1 className="modal__heading"> {project.name}</h1>
+			<p className="modal__summary">{project.description}</p>
 			<div className="modal__link-wrapper">
-				<a href="#!" className="modal__link modal__link--github">
-					<GithubIcon width={20} className="modal__link-icon" /> View on Github
-				</a>
-				<a href="#!" className="modal__link modal__link--live">
-					<LiveIcon width={20} className="modal__link-icon" /> View Live
-				</a>
+				{project.github_url ? (
+					<a
+						href={project.github_url}
+						target="_blank"
+						className="modal__link modal__link--github"
+					>
+						<GithubIcon width={20} className="modal__link-icon" /> View on
+						Github
+					</a>
+				) : (
+					<a />
+				)}
+				{project.live_url ? (
+					<a
+						href={project.live_url}
+						className="modal__link modal__link--live"
+						target="_blank"
+					>
+						<LiveIcon width={20} className="modal__link-icon" /> View Live
+					</a>
+				) : (
+					<a />
+				)}
 			</div>
 		</Rodal>
 	);
--- src/components/PortfolioPage/PortfolioPage.js
+++ src/components/PortfolioPage/PortfolioPage.js
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from "react";
 import "./PortfolioPage.css";
 import { AboutIcon, CloseIcon, ZoomIcon } from "../../images/icons/icons";
-import project from "../../images/project.png";
-import project2 from "../../images/project2.png";
 import data from "../../data.json";
 import Modal from "../Modal/Modal";
 
 const PortfolioPage = () => {
 	const [showModal, setShowModal] = useState(false);
 	const [selectedImg, setSelectedImg] = useState(null);
+	const [selectedProject, setSelectedProject] = useState(null);
 	const [showFullscreen, setShowFullscreen] = useState(false);
 
 	useEffect(() => {
@@ -18,93 +17,56 @@ const PortfolioPage = () => {
 
 	return (
 		<section className="portfolio-page">
-			<Modal visible={showModal} close={() => setShowModal(false)} />
+			<Modal
+				visible={showModal}
+				close={() => setShowModal(false)}
+				project={selectedProject}
+			/>
 			<header className="portfolio-page__header">
 				<h1 className="portfolio-page__heading portfolio-page__heading--main">
 					Recent Works
 				</h1>
 				<p className="portfolio-page__subheading">
-					Portfolio showcase of some work I've done
+					Portfolio showcase of some of my work.
 				</p>
 			</header>
 			<div className="portfolio-page__body">
-				{new Array(10).fill(0).map((item, i) => (
-					<div
-						className="portfolio-page__img-wrapper"
-						key={i}
-						// onClick={() => setShowModal(true)}
-					>
-						{i % 2 === 0 ? (
-							<>
-								<img
-									src={project}
-									alt="Project"
-									className="portfolio-page__img"
-								/>
-								<div className="portfolio-page__desc">
-									<h3> Scribbles Blog </h3>
-									<div className="portfolio-page__actions">
-										<button
-											className="portfolio-page__img-btn"
-											aria-label="View fullscreen"
-											title="View fullscreen"
-											onClick={() => setSelectedImg(project)}
-										>
-											<ZoomIcon
-												width={40}
-												className="portfolio-page__img-icon"
-											/>
-										</button>
-										<button
-											className="portfolio-page__img-btn"
-											aria-label="View details"
-											title="View details"
-											onClick={() => setShowModal(true)}
-										>
-											<AboutIcon
-												width={40}
-												className="portfolio-page__img-icon"
-											/>
-										</button>
-									</div>
+				{data.projects.map((project, i) => (
+					<div className="portfolio-page__img-wrapper" key={i}>
+						<>
+							<img
+								src={project.image_url}
+								alt={project.name}
+								className="portfolio-page__img"
+							/>
+							<div className="portfolio-page__desc">
+								<h3> {project.name} </h3>
+								<div className="portfolio-page__actions">
+									<button
+										className="portfolio-page__img-btn"
+										aria-label="View fullscreen"
+										title="View fullscreen"
+										onClick={() => setSelectedImg(project.image_url)}
+									>
+										<ZoomIcon width={40} className="portfolio-page__img-icon" />
+									</button>
+									<button
+										className="portfolio-page__img-btn"
+										aria-label="View details"
+										title="View details"
+										onClick={() => {
+											setSelectedProject(project);
+											setShowModal(true);
+										}}
+									>
+										<AboutIcon
+											width={40}
+											className="portfolio-page__img-icon"
+										/>
+									</button>
 								</div>
-							</>
-						) : (
-							<>
-								<img
-									src={project2}
-									alt="Project"
-									className="portfolio-page__img"
-								/>
-								<div className="portfolio-page__desc">
-									<h3> Scribbles Blog </h3>
-									<div className="portfolio-page__actions">
-										<button
-											className="portfolio-page__img-btn"
-											aria-label="View fullscreen"
-											title="View fullscreen"
-											onClick={() => setSelectedImg(project2)}
-										>
-											<ZoomIcon
-												width={40}
-												className="portfolio-page__img-icon"
-											/>
-										</button>
-										<button
-											className="portfolio-page__img-btn"
-											aria-label="View details"
-											title="View details"
-											onClick={() => setShowModal(true)}
-										>
-											<AboutIcon
-												width={40}
-												className="portfolio-page__img-icon"
-											/>
-										</button>
-									</div>
-								</div>
-							</>
-						)}
+							</div>
+						</>
 					</div>
 				))}
 
@@ -122,11 +84,7 @@ const PortfolioPage = () => {
 								className="portfolio-page__close-icon"
 							/>
 						</button>
-						<img
-							src={selectedImg}
-							alt="Project"
-							className="portfolio-page__img"
-						/>
+						<img src={selectedImg} alt="" className="portfolio-page__img" />
 					</div>
 				) : (
 					<div />

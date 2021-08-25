b8f0956146fa484cc300ecf5c5f6bea99e65cba5
Karen Okonkwo <karenokonkwo29@gmail.com>
2021-08-25 21:42:38 +0100

    Include scroll in animations - about page

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./AboutPage.css";
 import { Link } from "react-router-dom";
 import Lottie from "react-lottie";
+import Fade from "react-reveal/Fade";
 import animationData from "../../images/lotties/female.json";
 import { HTMLIcon, CodeBracesIcon, WebIcon } from "../../images/icons/icons";
 
@@ -30,78 +31,80 @@ const AboutPage = () => {
 						/>
 					</div>
 					<div className="about-page__content-right">
-						<p className="about-page__text about-page__text--restricted">
-							<strong className="about-page__text--bold"> Who am I? </strong>
-							<ul className="about-page__list">
-								<li className="about-page__list-item">
-									A Software Engineer with hands on experience developing
-									websites, full-stack web applications and APIs.
-								</li>
-								<li className="about-page__list-item">
-									Someone with a good grasp of engineering concepts, data
-									structures, algorithms and the design of systems.
-								</li>
-								<li className="about-page__list-item">
-									A Bachelor’s Degree (First Class Honours) holder in Computer
-									Engineering.
-								</li>
+						<Fade bottom>
+							<p className="about-page__text about-page__text--restricted">
+								<strong className="about-page__text--bold"> Who am I? </strong>
+								<ul className="about-page__list">
+									<li className="about-page__list-item">
+										A Software Engineer with hands on experience developing
+										websites, full-stack web applications and APIs.
+									</li>
+									<li className="about-page__list-item">
+										Someone with a good grasp of engineering concepts, data
+										structures, algorithms and the design of systems.
+									</li>
+									<li className="about-page__list-item">
+										A Bachelor’s Degree (First Class Honours) holder in Computer
+										Engineering.
+									</li>
 
-								<li className="about-page__list-item">
-									Currently making magic at{" "}
+									<li className="about-page__list-item">
+										Currently making magic at{" "}
+										<a
+											href="https://www.bloomberg.com/company/"
+											target="_blank"
+											className="about-page__link"
+										>
+											{" "}
+											Bloomberg L.P.
+										</a>
+									</li>
+								</ul>
+							</p>
+							<div className="about-page__text">
+								<strong className="about-page__text--bold"> What I do? </strong>
+								<div className="about-page__services">
 									<a
-										href="https://www.bloomberg.com/company/"
+										href="https://blog.udacity.com/2014/12/front-end-vs-back-end-vs-full-stack-web-developers.html"
 										target="_blank"
-										className="about-page__link"
+										className="about-page__services-link"
 									>
-										{" "}
-										Bloomberg L.P.
+										<HTMLIcon className="about-page__services-icon" />
+										<span className="about-page__services-text">
+											Front-End Web Development
+										</span>
 									</a>
-								</li>
-							</ul>
-						</p>
-						<div className="about-page__text">
-							<strong className="about-page__text--bold"> What I do? </strong>
-							<div className="about-page__services">
-								<a
-									href="https://blog.udacity.com/2014/12/front-end-vs-back-end-vs-full-stack-web-developers.html"
-									target="_blank"
-									className="about-page__services-link"
-								>
-									<HTMLIcon className="about-page__services-icon" />
-									<span className="about-page__services-text">
-										Front-End Web Development
-									</span>
-								</a>
-								<a
-									href="https://blog.udacity.com/2014/12/front-end-vs-back-end-vs-full-stack-web-developers.html"
-									target="_blank"
-									className="about-page__services-link"
-								>
-									<CodeBracesIcon className="about-page__services-icon" />
-									<span className="about-page__services-text">
-										Back-End / API Development
-									</span>
-								</a>
-								<a
-									href="https://blog.udacity.com/2014/12/front-end-vs-back-end-vs-full-stack-web-developers.html"
-									target="_blank"
-									className="about-page__services-link"
-								>
-									<WebIcon className="about-page__services-icon" />
-									<span className="about-page__services-text">
-										Full-Stack Web Development
-									</span>
-								</a>
+									<a
+										href="https://blog.udacity.com/2014/12/front-end-vs-back-end-vs-full-stack-web-developers.html"
+										target="_blank"
+										className="about-page__services-link"
+									>
+										<CodeBracesIcon className="about-page__services-icon" />
+										<span className="about-page__services-text">
+											Back-End / API Development
+										</span>
+									</a>
+									<a
+										href="https://blog.udacity.com/2014/12/front-end-vs-back-end-vs-full-stack-web-developers.html"
+										target="_blank"
+										className="about-page__services-link"
+									>
+										<WebIcon className="about-page__services-icon" />
+										<span className="about-page__services-text">
+											Full-Stack Web Development
+										</span>
+									</a>
+								</div>
 							</div>
-						</div>
 
-						<p className="about-page__text about-page__text--restricted">
-							I'm super excited you're here. Feel free to{" "}
-							<Link to="/contact" className="about-page__link">
-								reach out to me{" "}
-							</Link>
-							with any project ideas you have or to just say hello!
-						</p>
+							<p className="about-page__text about-page__text--restricted">
+								I'm super excited you're here. Feel free to{" "}
+								<Link to="/contact" className="about-page__link">
+									reach out to me{" "}
+								</Link>
+								with any project ideas you have or to just say hello!
+							</p>
+						</Fade>{" "}
 					</div>
 				</div>{" "}
 			</div>
